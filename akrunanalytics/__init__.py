f5abe7e16fdcd0cb1d3303f10306c44dddd25b6e
akrun1 <akrun111@gmail.com>
2025-02-25 13:47:53 -0500

    Add debug logging

--- akrunanalytics/__init__.py
+++ akrunanalytics/__init__.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template
+import os
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)
 
 # Create Flask app
 app = Flask(__name__)
@@ -6,6 +11,10 @@ app = Flask(__name__)
 # Basic routes
 @app.route('/')
 def home():
+    app.logger.debug(f'Template folder: {app.template_folder}')
+    app.logger.debug(f'Current directory: {os.getcwd()}')
+    app.logger.debug(f'Directory contents: {os.listdir(os.getcwd())}')
+    app.logger.debug(f'Templates directory contents: {os.listdir(app.template_folder)}')
     return render_template('index.html')
 
 @app.route('/test')
